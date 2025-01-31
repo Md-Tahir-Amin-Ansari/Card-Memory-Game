45dc4a9977b6d3a34d63a7a19cc883dba9af00bb
Md-Tahir-Amin-Ansari <mdtahiraminansari.work@gmail.com>
2025-01-31 21:58:07 +0530

    Plan Change: Removed traditional cards assets in favour of fallout theme cards.

---
+++ src/Grid.jsx
@@ -0,0 +1,26 @@
+import Card from "./Card";
+
+const Grid = () => {
+    function givePairsOfCards(){
+        
+    }
+
+    return ( 
+        <div className="grid">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+     );
+}
+ 
+export default Grid;
